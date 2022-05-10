618af752236233f1a61e40e0cab104f1c331816a
mbv1 <variousdisgraces@gmail.com>
2022-05-11 01:58:34 +0900

    Revert "스크립트 수정2"
    This reverts commit 5d2ee77a6e9642eb55a32328b28e01ae78d50b0f.

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,19 +5,20 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
+
 PROJECT_NAME=springboot-webservice
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 어플리케이션 배포"
+echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가"
+echo ">$JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
